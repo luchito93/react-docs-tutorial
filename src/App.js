2ba134df1e18f1e9dd53bd0fe3e5eb137a6c9b12
Luis <luisgomez@unisangil.edu.co>
2021-12-14 12:27:46 -0500

    fix: :bug: Update proyect and add react router fix postcss require for plugin

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { Suspense } from "react"
+
 import Calculator from "./Calculator"
 import Dialog from "./compositionvsinhritance/Dialog"
-import FilterableProductTable from "./thinkinginreact/FilterableProductTable"
+// import FilterableProductTable from "./thinkinginreact/FilterableProductTable"
 import FragmentsTable from "./advancedguides/fragments/Table"
 import RefExample from "./advancedguides/refsandthedom/MyComponent"
 import MouseAndPinterEvent from "./advancedguides/mouseandpointerevents/SelectExample"
+
+const FilterableProductTable = React.lazy(() => import("./thinkinginreact/FilterableProductTable"))
 
 function App (props) {
     return (
         <div className="container mx-auto my-3">
             <Calculator />
             <Dialog />
-            <FilterableProductTable />
+            <Suspense fallback={<div>Loading perros.......</div>}>
+                <FilterableProductTable />
+            </Suspense>
             <h3 className="font-bold text-center my-4">Advanced Guides</h3>
             <FragmentsTable />
             <RefExample />
